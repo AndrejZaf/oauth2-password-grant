8bfcac6ea30b1385e3f843bffce1bc5d88e87a56
Андреј Зафировски <Andrej.Zafirovski@students.finki.ukim.mk>
2021-08-03 11:11:58 +0200

    Added resource server

--- src/main/java/com/example/modernapi/security/ResourceServerConfig.java
+++ src/main/java/com/example/modernapi/security/ResourceServerConfig.java
@@ -1,23 +1,23 @@
-//package com.example.modernapi.security;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.token.TokenStore;
-//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-//
-//@Configuration
-//@EnableResourceServer
-//public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+package com.example.modernapi.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 //    @Value("${jwt.key}")
 //    private String jwtKey;
-//
+
 //    @Override
 //    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 //        resources.tokenStore(tokenStore());
@@ -34,4 +34,4 @@
 //        converter.setSigningKey(jwtKey);
 //        return converter;
 //    }
-//}
+}
